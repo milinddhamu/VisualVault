1d49c9df4e4a6fac30f7d222d86b3831d1f4dd26
Milind Dhamu <milind.dhamu.111@gmail.com>
2024-03-24 18:13:28 +0530

    [Refactor] : Navbar variables refactored

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,9 @@ function Navbar() {
   };
   const location = useLocation();
   const PagePath = location.pathname;
-  if(PagePath !== "/" && PagePath !== "/favorites"){
+  const isIndexPage = PagePath === "/"
+  const isfavoritesPage = PagePath === "/favorites"
+  if(!isIndexPage && !isfavoritesPage){
     return;
   }
 
@@ -19,15 +21,16 @@ function Navbar() {
     <div className='fixed z-10 flex w-full top-0 h-16 bg-black/50 backdrop-blur-xl max-w-screen-lg p-2 border-b-4 border-violet-500/50'>
       <div className='flex flex-row w-full gap-2'>
         <input 
-          placeholder={PagePath === '/' ? 'Search photos..' : 'Search my photos..'}
-          className='flex w-full p-4 focus:outline-none bg-black'
+          placeholder={isIndexPage ? 'Search photos..' : 'Search my photos..'}
+          className={`flex w-full p-4 focus:outline-none bg-black ${isfavoritesPage && "cursor-not-allowed"}`}
+          disabled={isfavoritesPage}
           onChange={handleInputChange}
           value={inputString}
           />
           <Link 
             to={PagePath === '/' ? '/favorites' : '/'}
             className='bg-violet-500 hover:bg-violet-700 transition-all w-full px-4 h-full flex items-center justify-center tracking-tighter font-semibold max-w-fit gap-2'>
-            {PagePath === '/' ? <><HiCollection /> My Favorites</> : <><HiHome /> Home</>}
+            {!isfavoritesPage ? <><HiCollection /> My Favorites</> : <><HiHome /> Home</>}
           </Link>
       </div>
     </div>
